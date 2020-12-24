bbcd8d966e1bdb3526a22ce64ca0b6658d5594eb
dai_phong <dai_phong@lampart-vn.com>
2020-12-24 12:19:42 +0700

    da thay doi

---
+++ package/foobar2/vendor/composer/installed.php
@@ -0,0 +1,60 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'larapen/package',
+  ),
+  'versions' => 
+  array (
+    'fideloper/proxy' => 
+    array (
+      'pretty_version' => '4.4.0',
+      'version' => '4.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9beebf48a1c344ed67c1d36bb1b8709db7c3c1a8',
+    ),
+    'illuminate/contracts' => 
+    array (
+      'pretty_version' => 'v8.20.1',
+      'version' => '8.20.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '073109b521aec104f11c5cf5aded97aa0e63f313',
+    ),
+    'larapen/package' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'psr/container' => 
+    array (
+      'pretty_version' => '1.0.0',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b7ce3b176482dbbc1245ebf52b181af44c2cf55f',
+    ),
+    'psr/simple-cache' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '408d5eafb83c57f6365a3ca330ff23aa4a5fa39b',
+    ),
+  ),
+);
